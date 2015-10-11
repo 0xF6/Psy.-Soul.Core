940b2b5ca19f6a8eb5c6165b5f661c4cbc4f5fe7
Yuuki Wesp <ls-micro@ya.ru>
2015-10-11 08:39:47 +0300

    a#2

--- PsySoul.Native/Source.cpp
+++ PsySoul.Native/Source.cpp
@@ -4,5 +4,10 @@
 
 void __inlineConnectToDevice(wchar_t* _IpS)
 {
-
+	//x SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_UDP);
+}
+const wchar_t* ____getVersionPLR(const wchar_t* key)
+{
+	if (key == L"634BF9EB-4AFE-4099-8E05-BB93710F9B35")
+		return L"0.2.20";
 }
